9c60e2e7a8c6e9a7ca71339d398db33f01ac1c16
ほと <mail@hoto.me>
2015-11-14 15:58:19 +0900

    退社通知の時間を制限

--- scripts/ifttt.js
+++ scripts/ifttt.js
@@ -10,6 +10,11 @@ module.exports = function(robot) {
 
   // 退社を通知
   ifttt.on('leftOffice', function(data) {
+    var date = new Date();
+    // 土日は通知しない
+    if ( date.getDay() % 6 === 0 ) { return; }
+    // 昼までは通知しない
+    if ( date.getHours() < 16 ) { return; }
     robot.send(options, '【' + data.description + '】ﾀｲｼｬ!!');
   });
 
