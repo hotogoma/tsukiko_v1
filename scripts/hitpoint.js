a8117e050c80beb289dd86542aa358b9d6e6b928
goma <gomayumax@gmail.com>
2015-11-19 11:09:02 +0900

    諸修正
    パターンマッチを英数字に限定、keyがない場合の値について対応、statusをjoin出力

--- scripts/hitpoint.js
+++ scripts/hitpoint.js
@@ -3,40 +3,38 @@ var hpMin = 0;
 var cron = require('../lib/cron');
 
 module.exports = function(robot) {
-  cron('0 * * * * *', function() {
+  cron('0 0 9 * * 7', function() {
     var list = robot.brain.data;
     for(var key in list._private){
       robot.brain.set(key, hpMax);
     }
   });
 
-  robot.respond(/attack (.+)/i, function(msg) {
+  robot.respond(/attack (\w+)/i, function(msg) {
     var user = msg.match[1];
-    var hp = robot.brain.get(user) - 10;
-    if (hp >= hpMin){
-      robot.brain.set(user, hp);
-    } else{
-      robot.brain.set(user, hpMin);
-    }
+    var hp = robot.brain.get(user) || hpMax;
+    hp = (hp !== null) ? hp : hpMax;
+    hp = Math.max(hp - 10, hpMin);
+    robot.brain.set(user, hp);
     msg.reply(user + 'は攻撃された' + 'HP' + robot.brain.get(user) + '/' + hpMax);
   });
 
-  robot.respond(/care (.+)/i, function(msg) {
+  robot.respond(/care (\w+)/i, function(msg) {
     var user = msg.match[1];
-    var hp = robot.brain.get(user) + 10;
-    if (hp < hpMax){
-      robot.brain.set(user, hp);
-    } else {
-      robot.brain.set(user, hpMax);
-    }
+    var hp = robot.brain.get(user);
+    hp = (hp !== null) ? hp : hpMax;
+    hp = Math.min(hp + 10, hpMax);
+    robot.brain.set(user, hp);
     msg.reply(user + 'は回復した' + 'HP' + robot.brain.get(user) + '/' + hpMax);
   });
 
   robot.respond(/status/, function(msg) {
     var list = robot.brain.data;
+    var status = new Array();
     for(var key in list._private){
-      msg.reply(key + ' HP:' + list._private[key] + '/' + hpMax);
+      status.push(key + ' HP:' + list._private[key] + '/' + hpMax);
     }
+    msg.reply(status.join("\n"));
   });
 };
 
