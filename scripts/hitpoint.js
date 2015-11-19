640fce89aece729b70c5a74807f436f72f9c1b6d
goma <gomayumax@gmail.com>
2015-11-19 11:44:45 +0900

    諸修正２回目！！
    曜日指定変更、msg.reply部分編集

--- scripts/hitpoint.js
+++ scripts/hitpoint.js
@@ -3,7 +3,7 @@ var hpMin = 0;
 var cron = require('../lib/cron');
 
 module.exports = function(robot) {
-  cron('0 0 9 * * 7', function() {
+  cron('0 0 9 * * 6', function() {
     var list = robot.brain.data;
     for(var key in list._private){
       robot.brain.set(key, hpMax);
@@ -12,11 +12,11 @@ module.exports = function(robot) {
 
   robot.respond(/attack (\w+)/i, function(msg) {
     var user = msg.match[1];
-    var hp = robot.brain.get(user) || hpMax;
+    var hp = robot.brain.get(user);
     hp = (hp !== null) ? hp : hpMax;
     hp = Math.max(hp - 10, hpMin);
     robot.brain.set(user, hp);
-    msg.reply(user + 'は攻撃された' + 'HP' + robot.brain.get(user) + '/' + hpMax);
+    msg.reply(user + 'は攻撃された' + 'HP' + hp + '/' + hpMax);
   });
 
   robot.respond(/care (\w+)/i, function(msg) {
@@ -25,7 +25,7 @@ module.exports = function(robot) {
     hp = (hp !== null) ? hp : hpMax;
     hp = Math.min(hp + 10, hpMax);
     robot.brain.set(user, hp);
-    msg.reply(user + 'は回復した' + 'HP' + robot.brain.get(user) + '/' + hpMax);
+    msg.reply(user + 'は回復した' + 'HP' + hp + '/' + hpMax);
   });
 
   robot.respond(/status/, function(msg) {
