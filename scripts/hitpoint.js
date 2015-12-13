d06eeae95083346348f17085ee227771e6c20213
ごま <gomayumax@users.noreply.github.com>
2015-12-13 17:22:23 +0900

    attack,care引数変更

--- scripts/hitpoint.js
+++ scripts/hitpoint.js
@@ -15,19 +15,15 @@ module.exports = function(robot) {
 
   robot.respond(/attack (\w+)/i, function(msg) {
     var user = msg.match[1];
-    var hp = robot.brain.get(user);
-    var point = 10;
-    hp = attack(hp, point);
-    robot.brain.set(user, hp);
-    msg.send(`${user}は攻撃された. ${point}のダメージ！\nHP: ${hp}/${hpMax}`);
+    var damage = 10;
+    hp = attack(user, damage);
+    msg.send(`${user}は攻撃された. ${damage}のダメージ！\nHP: ${hp}/${hpMax}`);
   });
 
   robot.respond(/care (\w+)/i, function(msg) {
     var user = msg.match[1];
-    var hp = robot.brain.get(user);
     var point = 10;
-    hp = care(hp, point);
-    robot.brain.set(user, hp);
+    var hp = care(user, point);
     msg.send(`${user}回復した. HP: ${hp}/${hpMax}`);
   });
 
@@ -44,39 +40,42 @@ module.exports = function(robot) {
     var user = msg.match[1];
     var magicNum = Math.floor(Math.random() * magic.length);
     var magicName = magic[magicNum].name;
-    var point = magic[magicNum].point;
-    var hp = robot.brain.get(user);
-    hp = attack(hp, point);
-    robot.brain.set(user, hp);
-    msg.send(`${user}は${magicName}で攻撃された. ${point}のダメージ！\nHP: ${hp}/${hpMax}`);
+    var damage = magic[magicNum].point;
+    var hp = attack(user, damage);
+    msg.send(`${user}は${magicName}で攻撃された. ${damage}のダメージ！\nHP: ${hp}/${hpMax}`);
   });
 
   // タイムラインを全て形態素解析する
   robot.hear(/.*/, function(msg) {
     if ( ! msg.message.tokenized ) { return; }
     var user = msg.message.user.name;
-    var hp = robot.brain.get(user);
-    var point = 5;
+    var damage = 5;
 
     // ネガティブワードをキャッチする
     msg.message.tokenized.forEach(function(token) {
       if ( /((疲|つか)れる)|((辛|つら)い)|((眠|ねむ)い)/.test(token.basic_form) ) {
-        hp = attack(hp, point);
-        robot.brain.set(user, hp);
-        msg.send(`${user}は社会から攻撃を受けた！${point}のダメージ！\nHP: ${hp}/${hpMax}`);
+        var hp = attack(user, damage);
+        msg.send(`${user}は社会から攻撃を受けた！${damage}のダメージ！\nHP: ${hp}/${hpMax}`);
       }
     });
   });
-};
 
-function attack(hp, damage){
+  function attack(user, damage){
+    var hp = robot.brain.get(user);
     hp = (hp !== null) ? hp : hpMax;
     hp = Math.max(hp - damage, hpMin);
+    robot.brain.set(user, hp);
     return hp;
-}
+  }
 
-function care(hp, point){
+  function care(user, point){
+    var hp = robot.brain.get(user);
     hp = (hp !== null) ? hp : hpMax;
     hp = Math.min(hp + 10, hpMax);
+    robot.brain.set(user, hp);
     return hp;
-}
+  }
+
+};
+
+
