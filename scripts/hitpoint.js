f8d65b82bb7b0c4b0e8439561a70106944d49d3d
ごま <gomayumax@users.noreply.github.com>
2016-01-30 17:02:14 +0900

    hp-managerを使うように変更

--- scripts/hitpoint.js
+++ scripts/hitpoint.js
@@ -6,40 +6,45 @@
 //   hubot status - ステータス表示
 //   hubot magic <username> - 魔法攻撃
 
-var hpMax = 100;
-var hpMin = 0;
 var cron = require('../lib/cron');
 var magic = require('../magic.json');
+var hpManager = require('hp-manager');
 var options = { room: process.env.SLACK_MAIN_CHANNEL };
 
 module.exports = function(robot) {
-  cron('0 0 9 * * 6', function() {
-    var list = robot.brain.data;
-    for(var key in list._private){
-      robot.brain.set(key, hpMax);
-    }
-    robot.send(options, '全回復しました');
-  });
+  var hpMane = new hpManager({ db: robot.brain});
 
   robot.respond(/attack (\w+)/i, function(msg) {
     var user = msg.match[1];
     var damage = 10;
-    var hp = attack(user, damage);
+    var hp = hpMane.attack(user ,damage);
     msg.send(`${user}は攻撃された. ${damage}のダメージ！\nHP: ${hp}/${hpMax}`);
   });
 
   robot.respond(/care (\w+)/i, function(msg) {
     var user = msg.match[1];
     var point = 10;
-    var hp = care(user, point);
+    var hp = hpMane.care(user ,point);
     msg.send(`${user}は${point}回復した. HP: ${hp}/${hpMax}`);
   });
 
-  robot.respond(/status/, function(msg) {
-    var list = robot.brain.data;
+  robot.respond(/status$/i, function(msg) {
+    var list = hpMane.status();
     var status = new Array();
-    for(var key in list._private){
-      status.push(`${key} HP: ${list._private[key]}/${hpMax}`);
+
+    for(var key in list){
+      status.push(`${key} HP: ${list[key]}/${hpMax}`);
+    }
+    msg.send(status.join("\n"));
+  });
+
+  robot.respond(/status (\w+)/i, function(msg) {
+    var user = msg.match[1];
+    var list = hpMane.status(user);
+    var status = new Array();
+
+    for(var key in list){
+      status.push(`${key} HP: ${list[key]}/${hpMax}`);
     }
     msg.send(status.join("\n"));
   });
@@ -49,41 +54,20 @@ module.exports = function(robot) {
     var magicNum = Math.floor(Math.random() * magic.length);
     var magicName = magic[magicNum].name;
     var damage = magic[magicNum].point;
-    var hp = attack(user, damage);
+    var hp = hpMane.attack(user ,damage);
     msg.send(`${user}は${magicName}で攻撃された. ${damage}のダメージ！\nHP: ${hp}/${hpMax}`);
   });
 
-  // タイムラインを全て形態素解析する
-  robot.hear(/.*/, function(msg) {
-    if ( ! msg.message.tokenized ) { return; }
-    var user = msg.message.user.name;
-    var damage = 5;
+  robot.respond(/delete (\w+)/i, function(msg) {
+    var user = msg.match[1];
+    var list = hpMane.delete(user);
+    var status = new Array();
 
-    // ネガティブワードをキャッチする
-    msg.message.tokenized.forEach(function(token) {
-      if ( /((疲|つか)れる)|((辛|つら)い)|((眠|ねむ)い)/.test(token.basic_form) ) {
-        var hp = attack(user, damage);
-        msg.send(`${user}は社会から攻撃を受けた！${damage}のダメージ！\nHP: ${hp}/${hpMax}`);
-      }
-    });
+    for(var key in list){
+      status.push(`${key} HP: ${list[key]}/${hpMax}`);
+    }
+    msg.send(status.join("\n"));
   });
 
-  function attack(user, damage){
-    var hp = robot.brain.get(user);
-    hp = (hp !== null) ? hp : hpMax;
-    hp = Math.max(hp - damage, hpMin);
-    robot.brain.set(user, hp);
-    return hp;
-  }
-
-  function care(user, point){
-    var hp = robot.brain.get(user);
-    hp = (hp !== null) ? hp : hpMax;
-    hp = Math.min(hp + 10, hpMax);
-    robot.brain.set(user, hp);
-    return hp;
-  }
 
 };
-
-
