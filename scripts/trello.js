ec9281448086a7bc0b8a58d7363f9f7eb2fc04e6
ごま <gomayumax@users.noreply.github.com>
2016-01-31 18:09:46 +0900

    日本語カードに対応

--- scripts/trello.js
+++ scripts/trello.js
@@ -6,7 +6,7 @@
 Trello = require("node-trello")
 
 module.exports = function(robot) {
-  robot.respond(/todo (\w+)/i, function(msg) {
+  robot.respond(/todo (.+)/i, function(msg) {
     var title = msg.match[1];
     trello = new Trello(process.env.HUBOT_TRELLO_KEY, process.env.HUBOT_TRELLO_TOKEN);
     trello.post("/1/cards", {name: title, idList: process.env.HUBOT_TRELLO_TODO}, function(err, data){
