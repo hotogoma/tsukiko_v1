5a817f5e4e3f2a7d788a27e9ac48d1672a4f10cc
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-03-29 20:22:27 +0900

    irkit.js を Arrow Functions に

--- scripts/irkit.js
+++ scripts/irkit.js
@@ -7,41 +7,41 @@
 
 var irkit = require('../lib/irkit');
 
-module.exports = function(robot) {
+module.exports = (robot) => {
 
-  var sendIR = function(msg, command, successMsg) {
+  function sendIR(msg, command, successMsg) {
     irkit.messages(
       command,
       function() { msg.send( successMsg ); },      // onSuccess
       function(errorMsg) { msg.send( errorMsg ); } // onError
     );
-  };
+  }
 
-  robot.respond(/テレビ(点|つ)けて/i, function(msg) {
+  robot.respond(/テレビ(点|つ)けて/i, (msg) => {
     sendIR(msg, 'tv_power', 'テレビをつけましたよ');
   });
 
-  robot.respond(/テレビ(消|け)して/i, function(msg) {
+  robot.respond(/テレビ(消|け)して/i, (msg) => {
     sendIR(msg, 'tv_power', 'テレビを消しましたよ');
   });
 
-  robot.respond(/エアコン(点|つ)けて/i, function(msg) {
+  robot.respond(/エアコン(点|つ)けて/i, (msg) => {
     sendIR(msg, 'aircon_on', 'エアコンをつけましたよ');
   });
 
-  robot.respond(/エアコン(消|け)して/i, function(msg) {
+  robot.respond(/エアコン(消|け)して/i, (msg) => {
     sendIR(msg, 'aircon_off', 'エアコンを消しましたよ');
   });
 
-  robot.respond(/Apple\s?TV/i, function(msg) {
+  robot.respond(/Apple\s?TV/i, (msg) => {
     sendIR(msg, 'appletv_select', 'AppleTV をつけましたよ');
   });
 
-  robot.respond(/(電気|照明|ライト)(点|つ)けて/i, function(msg) {
+  robot.respond(/(電気|照明|ライト)(点|つ)けて/i, (msg) => {
     sendIR(msg, 'light_on', '照明をつけましたよ');
   });
 
-  robot.respond(/(電気|照明|ライト)(消|け)して/i, function(msg) {
+  robot.respond(/(電気|照明|ライト)(消|け)して/i, (msg) => {
     sendIR(msg, 'light_off', '照明を消しましたよ');
   });
 
