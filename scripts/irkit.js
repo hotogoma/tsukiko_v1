bc95a1090df7edb4d83b3c64ee11deb2ceb883df
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-03-29 22:05:52 +0900

    IRKit まわりリファクタ

--- scripts/irkit.js
+++ scripts/irkit.js
@@ -5,44 +5,31 @@
 //   hubot エアコン(つけて|消して) - エアコンを操作
 //   hubot 電気(つけて|消して) - 照明を操作
 
-var irkit = require('../lib/irkit');
+var IRKit = require('../lib/irkit');
+var signals = require('../config/irkit.json');
 
 module.exports = (robot) => {
 
-  function sendIR(msg, command, successMsg) {
-    irkit.messages(
-      command,
-      function() { msg.send( successMsg ); },      // onSuccess
-      function(errorMsg) { msg.send( errorMsg ); } // onError
-    );
-  }
+  var irkit = new IRKit();
 
-  robot.respond(/テレビ(点|つ)けて/i, (msg) => {
-    sendIR(msg, 'tv_power', 'テレビをつけましたよ');
-  });
+  if ( ! irkit.available() ) { return; }
 
-  robot.respond(/テレビ(消|け)して/i, (msg) => {
-    sendIR(msg, 'tv_power', 'テレビを消しましたよ');
-  });
+  var patterns = [
+    { regex: /テレビ(点|つ)けて/i,             signal: signals.tv.power,       message: 'テレビをつけましたよ'   },
+    { regex: /テレビ(消|け)して/i,             signal: signals.tv.power,       message: 'テレビを消しましたよ'   },
+    { regex: /エアコン(点|つ)けて/i,           signal: signals.aircon.on,      message: 'エアコンをつけましたよ' },
+    { regex: /エアコン(消|け)して/i,           signal: signals.aircon.off,     message: 'エアコンを消しましたよ' },
+    { regex: /Apple\s?TV/i,                    signal: signals.appletv.select, message: 'AppleTV をつけましたよ' },
+    { regex: /(電気|照明|ライト)(点|つ)けて/i, signal: signals.light.on,       message: '照明をつけましたよ'     },
+    { regex: /(電気|照明|ライト)(消|け)して/i, signal: signals.light.off,      message: '照明を消しましたよ'     },
+  ];
 
-  robot.respond(/エアコン(点|つ)けて/i, (msg) => {
-    sendIR(msg, 'aircon_on', 'エアコンをつけましたよ');
-  });
-
-  robot.respond(/エアコン(消|け)して/i, (msg) => {
-    sendIR(msg, 'aircon_off', 'エアコンを消しましたよ');
-  });
-
-  robot.respond(/Apple\s?TV/i, (msg) => {
-    sendIR(msg, 'appletv_select', 'AppleTV をつけましたよ');
-  });
-
-  robot.respond(/(電気|照明|ライト)(点|つ)けて/i, (msg) => {
-    sendIR(msg, 'light_on', '照明をつけましたよ');
-  });
-
-  robot.respond(/(電気|照明|ライト)(消|け)して/i, (msg) => {
-    sendIR(msg, 'light_off', '照明を消しましたよ');
+  patterns.forEach((pattern) => {
+    robot.respond(pattern.regex, (msg) => {
+      irkit.send( pattern.signal )
+        .then(() => msg.send( pattern.message ))
+        .catch((errMsg) => msg.send( errorMsg ));
+    });
   });
 
 };
