46d6cf1086afbd0d752569e123b1c863823d6c9c
ほと <mail@hoto.me>
2016-05-01 17:19:07 +0900

    URL を script 側に移動

--- scripts/meshi.js
+++ scripts/meshi.js
@@ -6,16 +6,25 @@
 
 let Cookpad = require('../lib/cookpad');
 
+const BARGAINS = [
+  {
+    shop: '文化堂 戸越銀座店',
+    url: 'https://cookpad.com/bargains/%E6%96%87%E5%8C%96%E5%A0%82/4114',
+  },
+];
+
 module.exports = (robot) => {
 
   robot.respond(/特売$/i, (msg) => {
-    Cookpad.bargains().then((bargains) => {
+    var bargains = BARGAINS[0];
+    Cookpad.bargains(bargains.url).then((items) => {
       var text = [
         `今日の ${bargains.shop} の特売情報ですよ`,
         bargains.url,
       ];
-      var items = bargains.items.map((item) => '・ ' + item);
-      msg.send( text.concat(items).join("\n") );
+      items = items.map((item) => '・ ' + item);
+      text = text.concat(items).join("\n");
+      msg.send(text);
     });
   });
 
